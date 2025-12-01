a803c4b10f0d34e13286ce4cf75993790f63bab1
BuddhikaBICT-UoR-FoT-6 <buddhikadarshan1475@gmail.com>
2025-12-01 14:00:00 +0530

    chore(backend): scaffold Spring Boot 3 Maven project with package structure

---
+++ optitrack-backend/src/main/java/com/optitrack/OptitrackBackendApplication.java
@@ -0,0 +1,13 @@
+package com.optitrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OptitrackBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OptitrackBackendApplication.class, args);
+	}
+
+}
